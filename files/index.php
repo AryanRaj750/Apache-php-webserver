d7c8afbc188d68aa1b6cf2ab6d0bd167f0b98f3c
Aryan Raj <ar9131000@gmail.com>
2021-04-13 02:07:18 +0530

    apache-php webserver

---
+++ files/index.php
@@ -0,0 +1,12 @@
+<pre>
+<?php
+print'<h1>';
+print'welcome to my world!!!'; 
+
+print'</h1><h3><br> <br>';
+print'successfully configured webserver.....';
+print'</h3>';
+print'<br> <br>';
+print`/usr/sbin/ifconfig`;
+?>
+</pre>
